b77ebddaaa90b6b9b58eb5c674f596bb48a66e7e
Sumit Bashetwar <84890708+Sumitbashetwar@users.noreply.github.com>
2022-05-16 12:30:25 +0530

    Add files via upload

---
+++ expt8/Databasecreation.java
@@ -0,0 +1,26 @@
+package expt8;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Databasecreation {
+	
+	static final String DB_URL = "jdbc:mysql://localhost:3306/"	;
+	static final String USER = "root";
+	static final String PASS = "sumitbashetwar";
+	
+	public static void main(String[] args) {
+// Open a connection
+		try(Connection conn = DriverManager.getConnection(DB_URL, USER,PASS);
+				Statement stmt = conn.createStatement();) {
+			String sql = "CREATE DATABASE STUDENTS1";
+			stmt.executeUpdate(sql);
+			System.out.println("Database created successfully...");
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
